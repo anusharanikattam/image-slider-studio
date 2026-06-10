186f478cff3bd35d371be59453e216f764937a4c
anusharanikattam <165913529+anusharanikattam@users.noreply.github.com>
2026-06-10 11:11:44 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const slides=document.querySelectorAll('.slide');
-let current=0;
-function show(i){slides.forEach(s=>s.classList.remove('active'));slides[i].classList.add('active');}
-document.querySelector('.next').onclick=()=>{current=(current+1)%slides.length;show(current)}
-document.querySelector('.prev').onclick=()=>{current=(current-1+slides.length)%slides.length;show(current)}
-setInterval(()=>{current=(current+1)%slides.length;show(current)},3000);
-window.onload=()=>document.getElementById('loader').style.display='none';
+window.addEventListener('load',()=>{document.getElementById('loader').style.display='none';});
